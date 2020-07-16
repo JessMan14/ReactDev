f1ddb7dfd9465d4647de3ad1b4e81f43c78e418f
Jessie <jasuncion@willdan.com>
2020-07-16 16:23:29 -0700

    Nothing

--- src/Components/Charts/Charts.jsx
+++ src/Components/Charts/Charts.jsx
@@ -202,7 +202,6 @@ export default class Charts extends Component {
                     />                    
                 </div>
                 <div id="LinesChart" style={{ width: "100%", height: "400px" }}></div>
-
                 <div>
                     <table>
                     <tbody>
